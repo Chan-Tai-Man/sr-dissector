fa1f1a3302651ae76a83b0e12deac15b08984779
Tim Hagemann <tim@eternalwings.de>
2022-10-22 15:50:20 +0200

    Refactor to handle multiple/split packets
    Previous iteration only handled one "frame" per tcp packet, but there
    could obviously be more than one. The dissector would then only handle
    the first frame, while ignoring everything that comes after. Or the tcp
    packet could be cut in the middle of a very large silkroad frame,
    causing the dissector to mess up the following packet as the header
    would obviously be wrong (since it now contains data).
    
    To handle the former, we can just run the dissector multiple times on
    the same buffer, adding new tree information for each frame handled this
    way. Each iteration, we keep track of how many bytes we've already used
    (the offset) and work from there. In most cases, this will already get
    us to the finish line, but this now makes us more open the second case.
    To solve that, we can tell wireshark that it needs to desegment the
    packets to create a full frame with exactly how many bytes we're
    missing. It will then go ahead and append the next packet until we
    finally finish.

--- silkroad/SERVER_GLOBAL_HANDSHAKE.lua
+++ silkroad/SERVER_GLOBAL_HANDSHAKE.lua
@@ -1,18 +1,18 @@
 local SERVER_GLOBAL_HANDSHAKE = {}
 
-function SERVER_GLOBAL_HANDSHAKE.parse(buffer)
-	SERVER_GLOBAL_HANDSHAKE.Flag = buffer(6, 1)
+function SERVER_GLOBAL_HANDSHAKE.parse(buffer, offset)
+	SERVER_GLOBAL_HANDSHAKE.Flag = buffer(offset + 6, 1)
 	if SERVER_GLOBAL_HANDSHAKE.Flag:le_uint() == 0x0E then
-		SERVER_GLOBAL_HANDSHAKE.Blowfish = buffer(7, 8)
-		SERVER_GLOBAL_HANDSHAKE.CountSeed = buffer(15, 4)
-		SERVER_GLOBAL_HANDSHAKE.CRCSeed = buffer(19, 4)
-		SERVER_GLOBAL_HANDSHAKE.Seed_1 = buffer(23, 4)
-		SERVER_GLOBAL_HANDSHAKE.Seed_2 = buffer(27, 4)
-		SERVER_GLOBAL_HANDSHAKE.Seed_3 = buffer(31, 4)
-		SERVER_GLOBAL_HANDSHAKE.Seed_4 = buffer(35, 4)
-		SERVER_GLOBAL_HANDSHAKE.Seed_5 = buffer(39, 4)
+		SERVER_GLOBAL_HANDSHAKE.Blowfish = buffer(offset + 7, 8)
+		SERVER_GLOBAL_HANDSHAKE.CountSeed = buffer(offset + 15, 4)
+		SERVER_GLOBAL_HANDSHAKE.CRCSeed = buffer(offset + 19, 4)
+		SERVER_GLOBAL_HANDSHAKE.Seed_1 = buffer(offset + 23, 4)
+		SERVER_GLOBAL_HANDSHAKE.Seed_2 = buffer(offset + 27, 4)
+		SERVER_GLOBAL_HANDSHAKE.Seed_3 = buffer(offset + 31, 4)
+		SERVER_GLOBAL_HANDSHAKE.Seed_4 = buffer(offset + 35, 4)
+		SERVER_GLOBAL_HANDSHAKE.Seed_5 = buffer(offset + 39, 4)
 	elseif SERVER_GLOBAL_HANDSHAKE.Flag:le_uint() == 0x10 then
-		SERVER_GLOBAL_HANDSHAKE.Blowfish = buffer(7, 8)
+		SERVER_GLOBAL_HANDSHAKE.Blowfish = buffer(offset + 7, 8)
 	end
 end
 
--- sr-dissector.lua
+++ sr-dissector.lua
@@ -13,37 +13,77 @@ header.opcode = ProtoField.uint16("silkroad.header.opcode", "Opcode", base.HEX)
 header.security = ProtoField.new("Security", "silkroad.header.security", ftypes.UINT16)
 
 function silkroad_protocol.dissector(buffer, pinfo, tree)
-	local size = buffer(0,2)
-	local opcode = buffer(2,2)
-	local security = buffer(4,2)
+	pinfo.cols.protocol = "Silkroad"
+
+	local pklen = buffer:len()
+	local current_offset = 0
+	local info = ""
+	while current_offset < pklen do
+		local bytes_consumed, pkt_info = dissect_single_packet(buffer, tree, current_offset)
+		if bytes_consumed < 0 then
+			pinfo.desegment_offset = current_offset
+			pinfo.desegment_len = -bytes_consumed
+			return current_offset + (-bytes_consumed)
+		elseif bytes_consumed == 0 then
+			return 0
+		else
+			if info:len() == 0 then
+				info = pkt_info
+			else
+				info = info .. " - " .. pkt_info
+			end
+			current_offset = current_offset + bytes_consumed
+			pinfo.cols.info:set(info)
+		end
+	end
+
+	return current_offset
+end
+
+function dissect_single_packet(buffer, tree, offset)
+	local remaining = buffer:len() - offset
+	if remaining < 6 then
+		-- if we have less than 6 bytes remaining, we say we need the different of bytes
+		-- before we can continue. We negate this to differentiate between success and failure.
+		return -DESEGMENT_ONE_MORE_SEGMENT, ""
+	end
+
+	local info = ""
+	local size = buffer(offset,2)
+	local total_size = size:le_uint() + 6
+	local opcode = buffer(offset + 2,2)
+	local security = buffer(offset + 4,2)
 	
-	local is_encrypted = buffer(1,1):bitfield(0,1) == 1
+	local is_encrypted = buffer(offset + 1,1):bitfield(0,1) == 1
 	if is_encrypted then
 		local size_encrypted = size:le_uint()
 		size = size_encrypted - 0x8000
+		total_size = get_encrypted_aligned_data_size(size) + 6
+	end
+
+	if remaining < total_size then
+		return -(total_size - remaining), ""
 	end
 	
-	pinfo.cols.protocol = "Silkroad"
+	local TREE_MAIN = tree:add(silkroad_protocol, buffer(offset, total_size), "Silkroad Protocol")
 	
-	local TREE_MAIN = tree:add(silkroad_protocol, buffer(0,-1), "Silkroad Protocol")
-	
-	local SUBTREE_HEADER = TREE_MAIN:add(silkroad_protocol, buffer(0,6), "Packet Header")
+	local SUBTREE_HEADER = TREE_MAIN:add(silkroad_protocol, buffer(offset,6), "Packet Header")
 	SUBTREE_HEADER:add_le(header.datasize, size);
 	SUBTREE_HEADER:add_le(header.opcode, opcode);
 	SUBTREE_HEADER:add_le(header.security, security);
 	
 	-- Put the opcode inside "Info" column.
-	pinfo.cols.info = "0x" .. string.upper(tostring(opcode)(3,4)) .. string.upper(tostring(opcode)(0,2));
+	info = "0x" .. string.upper(tostring(opcode)(3,4)) .. string.upper(tostring(opcode)(0,2));
 	
-	local SUBTREE_DATA = TREE_MAIN:add(silkroad_protocol, buffer(6, -1), "Packet Data")
+	local SUBTREE_DATA = TREE_MAIN:add(silkroad_protocol, buffer(offset + 6, total_size - 6), "Packet Data")
 	
 	if is_encrypted then
-		pinfo.cols.info:append(" #################################################")
+		info = info .. " ENCRYPTED"
 	else
-		local opcode = buffer(2,2):le_uint()
+		local opcode = buffer(offset + 2,2):le_uint()
 
 		if opcode == 0x2001 then
-			SERVER_GLOBAL_IDENTIFICATION.parse(buffer)
+			SERVER_GLOBAL_IDENTIFICATION.parse(buffer, offset)
 			SUBTREE_DATA:add(SERVER_GLOBAL_IDENTIFICATION.ServiceName, "ServiceName: " .. SERVER_GLOBAL_IDENTIFICATION.ServiceName);
 			if SERVER_GLOBAL_IDENTIFICATION.ServiceType:le_uint() == True then
 				SUBTREE_DATA:add(SERVER_GLOBAL_IDENTIFICATION.ServiceType, "ServiceType: module - module");
@@ -52,7 +92,7 @@ function silkroad_protocol.dissector(buffer, pinfo, tree)
 			end
 			opcodeDesc = "** SERVICE_IDENTIFICATION **"
 		elseif opcode == 0x5000 then
-			SERVER_GLOBAL_HANDSHAKE.parse(buffer)
+			SERVER_GLOBAL_HANDSHAKE.parse(buffer, offset)
 			SUBTREE_DATA:add_le(SERVER_GLOBAL_HANDSHAKE.Flag, "Flag: " .. SERVER_GLOBAL_HANDSHAKE.Flag)
 			if SERVER_GLOBAL_HANDSHAKE.Flag:le_uint() == 0x0E then
 				SUBTREE_DATA:add_le(SERVER_GLOBAL_HANDSHAKE.Blowfish, "Blowfish: " .. SERVER_GLOBAL_HANDSHAKE.Blowfish)
@@ -70,8 +110,19 @@ function silkroad_protocol.dissector(buffer, pinfo, tree)
 		else
 			opcodeDesc = OPCODES.get_description(opcode)
 		end
-		pinfo.cols.info:append(" " .. opcodeDesc)
+		info = info .. " " .. opcodeDesc
 	end
+
+	return total_size, info
+end
+
+function get_encrypted_aligned_data_size(size)
+	local bytes_to_encrypt = size + 4 -- Part of the header is also encrypted
+	local missing_for_alignment = bytes_to_encrypt % 8
+	if missing_for_alignment > 0 then
+		return (bytes_to_encrypt + (8 - missing_for_alignment)) - 4
+	end
+	return size
 end
 
 tcp_table = DissectorTable.get("tcp.port")
